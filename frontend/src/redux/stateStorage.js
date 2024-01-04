bbe4fefa023902e30f035ed99d5db16ba4c367be
DAMOCLOIDS <khoa27012000@gmail.com>
2024-01-04 10:46:47 +0700

    style: add thuChi toggle redux state

--- frontend/src/redux/stateStorage.js
+++ frontend/src/redux/stateStorage.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import moneyInputReducer from './features/moneyInput/moneyInputSlice'
+import thuChiReducer from './features/thuChi/thuChiSlice'
 
 export const stateStorage = configureStore({
-  reducer: { moneyInput: moneyInputReducer }
+  reducer: { moneyInput: moneyInputReducer, thuChi: thuChiReducer }
 })
